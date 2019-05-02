c510b3676d90b02b9f94b8f8d54a2e2c61338ded
Franck Michea <franck.michea@gmail.com>
2019-05-02 20:15:21 +0200

    Add footer for the project.

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -20,6 +20,17 @@ export class MainLayout extends PureComponent {
                     <ScoreHistory />
                 </Col>
             </Row>
+
+            <Row>
+                <Col md={12}>
+                    <p className="footer">
+                        Made by <a href="https://twitter.com/98810f8c06" className="discreet-link">Franck Michea</a> &mdash;{' '}
+                        <a href="https://github.com/fmichea/chess-score-tracker" className="discreet-link">
+                            <i className="fa fa-github"></i> Sources
+                        </a>
+                    </p>
+                </Col>
+            </Row>
         </Container>
     }
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { App } from './App';
 import * as serviceWorker from './serviceWorker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
