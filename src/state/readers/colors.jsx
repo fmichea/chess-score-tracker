a9955d8ca689e3c6a4716fbe43835abaaf9c223b
Franck Michea <franck.michea@gmail.com>
2019-05-05 21:02:47 +0200

    Return null color when it is unknown in redux state.

--- src/state/readers/colors.jsx
+++ src/state/readers/colors.jsx
@@ -1,5 +1,5 @@
 import {pick} from "../../lib/pick";
 
 export const getColorByColorID = (state, colorID) => {
-    return pick(state.colors[colorID], {})
-}
+    return pick(state.colors[colorID]);
+};
