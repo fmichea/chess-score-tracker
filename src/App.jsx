1149effa5ba428f978fc6d9750500d6ef0a53a4f
Franck Michea <franck.michea@gmail.com>
2019-05-05 20:59:34 +0200

    Move store creation to its own function.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React, {PureComponent} from "react";
 import {Provider} from "react-redux";
-import {createStore} from "redux";
-import {reducers} from "./state/reducers";
 
 import {MainLayout} from "./components/MainLayout";
+import {createAppStore} from "./state/store";
 
 export class App extends PureComponent {
   render() {
-    const store = createStore(reducers);
+    const store = createAppStore();
 
     return <Provider store={store}>
       <MainLayout />
